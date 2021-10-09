5c84a8fd55502f1a46442c8ccbd756c7f44eef7f
WeisgerberEstelle <89032759+WeisgerberEstelle@users.noreply.github.com>
2021-10-09 18:42:03 +0200

    Create Car.php

---
+++ Car.php
@@ -0,0 +1,104 @@
+<?php 
+    
+    class Car
+    {
+        private string $color;
+
+        private int $currentSpeed;
+
+        public int $nbSeats;
+
+        public int $nbWheels;
+
+        public string $energy;
+
+        public int $energyLevel;
+
+        public function __construct(string $color, int $nbSeats, string $energy)
+            {
+                
+                $this->color = $color;
+                $this->nbSeats= $nbSeats;
+                $this->energy = $energy;
+
+            }
+            
+
+        public function brake(): string
+            {
+
+                $sentence = "";
+
+                while ($this->currentSpeed > 0) {
+
+                    $this->currentSpeed--;
+                    $sentence .= "Brake !!!";
+
+                }
+
+                $sentence .= "I'm stopped !";
+                return $sentence;
+
+            }
+
+        public function forward(): string
+            {
+            
+                $this->currentSpeed = 15;          
+                return "Go !";   
+
+            }
+        public function start($currentSpeed): string
+            {
+            
+                if($currentSpeed = 0){         
+                    return "Démarre la voiture !";   
+                }
+            }
+
+        public function getColor(): string
+            {
+            
+                return $this->color;
+            
+            }
+
+        public function getCurrentSpeed(): int
+            {
+            
+                return $this->currentSpeed;
+            
+            } 
+
+        public function getNbWheels(): int
+            {
+            
+                return $this->nbWheels;
+            
+            }
+
+        public function getNbSeats(): int
+            {
+            
+                return $this->nbSeats;
+            
+            }
+        
+        public function getEnergy(): int
+            {
+            
+                return $this->energy;
+            
+            } 
+
+        public function getEnergyLevel(): int
+            {
+            
+                return $this->energyLevel;
+            
+            } 
+                     
+              
+    }
+
+   ?>
